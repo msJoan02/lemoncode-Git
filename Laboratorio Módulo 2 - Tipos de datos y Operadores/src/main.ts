b6fd21a79cc5c2cf7c73e24ef2e3af767ac40656
msJoan02 <39194596+msJoan02@users.noreply.github.com>
2023-12-17 18:57:44 +0100

    Update main.ts

--- Laboratorio Módulo 2 - Tipos de datos y Operadores/src/main.ts
+++ Laboratorio Módulo 2 - Tipos de datos y Operadores/src/main.ts
@@ -1,3 +1,14 @@
-import "./style.css";
+// Definir el costo total del ticket y el costo de las bebidas incluidas
+const costoTotal = 120;
+const costoBebidasIncluidas = 18;
 
-console.log("Hello Typescript!");
+// Número de amigos
+const numeroAmigos = 6;
+
+// Calcular el costo total sin las bebidas
+const costoCena = costoTotal - costoBebidasIncluidas;
+
+// Calcular el costo por persona
+const costoPorPersona = costoCena / numeroAmigos;
+
+console.log(`Cada comensal debe pagar ${costoPorPersona.toFixed(2)}€ para dividir los costos de manera equitativa.`);
