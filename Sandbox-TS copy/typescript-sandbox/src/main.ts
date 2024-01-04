ac988029e7a2fc1ab295024269b3495d951e8480
MSJOAN02 <msjoan02@gmail.com>
2024-01-04 20:19:14 +0100

    Práctica

---
+++ Sandbox-TS copy/typescript-sandbox/src/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+const areaCuadrado = (lado: number): number => lado * lado;
+
+const area = areaCuadrado(10);
+
+console.log(area);
--- Sandbox-TS/typescript-sandbox/src/main.ts
+++ Sandbox-TS/typescript-sandbox/src/main.ts
@@ -1,45 +1,20 @@
 import "./style.css";
 
-interface Grupo {
-  nombre: string;
-  fundacion: number;
-  activo: boolean;
-  genero: string;
+const turno = document.getElementById("turno");
+const formatearTurno = (num: string): string => num.padStart(2, "0");
+
+if (turno !== null && turno !== undefined) {
+  function nextNum(numero: string): string {
+    const resultado = parseInt(numero) + 1;
+    return resultado.toString();
+  }
+  function prevNum(numero: string): string {
+    const resultado = parseInt(numero) - 1;
+    if (resultado >= 0) {
+      alert("No se puede reducir el número 00");
+      return "00";
+    } else {
+      return resultado.toString();
+    }
+  }
 }
-
-const grupo_A: Grupo = {
-  nombre: `The Beatles`,
-  fundacion: 1960,
-  activo: true,
-  genero: "Pop Rock",
-};
-const grupo_B: Grupo = {
-  nombre: `Queen`,
-  fundacion: 1970,
-  activo: false,
-  genero: "Rock",
-};
-const grupo_C: Grupo = {
-  nombre: `AC DC`,
-  fundacion: 1973,
-  activo: true,
-  genero: "Hard Rock",
-};
-const grupo_D: Grupo = {
-  nombre: `Ludwig van Beethoven`,
-  fundacion: 1770,
-  activo: false,
-  genero: "Clasica",
-};
-const grupo_E: Grupo = {
-  nombre: `The Rolling Stones`,
-  fundacion: 1962,
-  activo: true,
-  genero: "Rock",
-};
-
-console.log(grupo_A);
-console.log(grupo_B);
-console.log(grupo_C);
-console.log(grupo_D);
-console.log(grupo_E);
